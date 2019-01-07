4921d14977fd832a5685b17340ce51857c2f4037
TRINHPAPA\HoangTrinh <hoangtrinh1610@gmail.com>
2019-01-07 23:12:10 +0700

    1

--- assets/js/mediaplayer.js
+++ assets/js/mediaplayer.js
@@ -1,12 +1,15 @@
-var area_player = document.querySelector("#media-player");
+var media_player = document.querySelector("#media-player");
 var video_area = document.querySelector(".video-area");
 var video_player = document.querySelector("#video-player");
-var toggle_play_pause = document.getElementById("togglePlay");
-var timeline_bar = document.querySelector(".video-process-area");
-var realtime_bar = document.querySelector(".video-process-realtime");
-var buffered_bar = document.querySelector(".video-process-buffered");
-var time_video = document.getElementById("videoTimer");
+var toggle_play_pause = document.getElementById("toggle-play-pause");
+var change_volume = document.getElementById("change-volume");
+var process_bar = document.querySelector(".process-area");
+var process_buffered_bar = document.querySelector(".process-buffered");
+var process_currenttime_bar = document.querySelector(".process-currenttime");
+var video_timer = document.querySelector(".video-timer");
+var pixel_button = document.querySelectorAll(".pixel-button");
 var currentVolume = 0;
+var timeoutMouse = null;
 
 function togglePlayPause() {
     if (video_player.paused) {
@@ -18,17 +21,23 @@ function togglePlayPause() {
     }
 }
 
-var i = null;
-$("#media-player").mousemove(function () {
-    clearTimeout(i);
-    $(".video-control-area").fadeIn(300)
-    i = setTimeout(function () {
-        $(".video-control-area").fadeOut(300);
-    }, 5000);
-}).mouseleave(function () {
-    clearTimeout(i);
-    $(".video-control-area").fadeOut(300);
-});
+function toggleMuteUnmute() {
+    if (video_player.volume === 0) {
+        change_volume.innerHTML = '<i class="fa fa-volume-up aria-hidden="true"></i>';
+        video_player.volume = 1;
+    } else {
+        change_volume.innerHTML = '<i class="fa fa-volume-off" aria-hidden="true"></i>';
+        video_player.volume = 0;
+    }
+}
+
+function toggleLight() {
+    if ($(".video-area").hasClass("light-off")) {
+        $(".video-area").removeClass("light-off");
+    } else {
+        $(".video-area").addClass("light-off");
+    }
+}
 
 function convertSecondsToTime(time) {
     let h = Math.floor(time / 3600);
@@ -37,41 +46,73 @@ function convertSecondsToTime(time) {
     return (h == 0 ? "" : h + ":") + m + ":" + (s < 10 ? "0" + s : s);
 }
 
+function setUrlMedia(src, currentTime = 0) {
+    video_player.setAttribute("src", src);
+    video_player.addEventListener("loadedmetadata", function () {
+        this.currentTime = currentTime;
+    }, false);
+}
+
+
+$("#media-player").mousemove(function () {
+    clearTimeout(timeoutMouse);
+    $(".media-control-area").fadeIn(300)
+    timeoutMouse = setTimeout(function () {
+        $(".media-control-area").fadeOut(300);
+    }, 5000);
+}).mouseleave(function () {
+    clearTimeout(timeoutMouse);
+    $(".media-control-area").fadeOut(300);
+});
+
+
+$("#toggle-light").click(function () {
+    toggleLight();
+});
+
+function changePixel(e) {
+    let currentTimeOld = video_player.currentTime;
+    video_player.setAttribute("src", e.dataset.url);
+    video_player.addEventListener("loadedmetadata", function () {
+        video_player.currentTime = currentTimeOld;
+    }, false);
+    video_player.play();
+};
+
 video_area.ondblclick = function () {
     if (document.fullscreenElement) {
         document.exitFullscreen();
     } else {
-        area_player.requestFullscreen();
+        media_player.requestFullscreen();
     }
 };
 
-toggle_play_pause.onclick = function () {
+video_area.onclick = function () {
     togglePlayPause();
 };
 
 video_player.addEventListener("timeupdate", function () {
-    realtime_bar.style.width =
-        (video_player.currentTime / video_player.duration) * 100 + "%";
-    time_video.innerText =
-        convertSecondsToTime(video_player.currentTime) +
-        " / " +
-        convertSecondsToTime(video_player.duration);
+    process_currenttime_bar.style.width = (video_player.currentTime / video_player.duration) * 100 + "%";
+    video_timer.innerText = convertSecondsToTime(video_player.currentTime) + " / " + convertSecondsToTime(video_player.duration);
 });
 
 video_player.addEventListener("progress", function () {
-    buffered_bar.style.width = ((video_player.buffered.end(0) / video_player.duration) * 100 + "%");
+    process_buffered_bar.style.width = ((video_player.buffered.end(0) / video_player.duration) * 100 + "%");
 });
 
 video_area.onclick = function () {
     togglePlayPause();
 };
 
-timeline_bar.onclick = function (e) {
+process_bar.onclick = function (e) {
     let timePos = e.pageX - this.getBoundingClientRect().left;
-    realtime_bar.style.width = (timePos / this.offsetWidth) * 100 + "%";
-    video_player.currentTime =
-        (timePos * video_player.duration) / this.offsetWidth;
+    process_currenttime_bar.style.width = (timePos / this.offsetWidth) * 100 + "%";
+    video_player.currentTime = (timePos * video_player.duration) / this.offsetWidth;
 };
+
+change_volume.onclick = function () {
+    toggleMuteUnmute();
+}
 
 document.onkeyup = function (e) {
     if (e.keyCode === 32) {
@@ -93,7 +134,7 @@ document.onkeyup = function (e) {
         if (document.fullscreenElement) {
             document.exitFullscreen();
         } else {
-            area_player.requestFullscreen();
+            media_player.requestFullscreen();
         }
     }
     if (e.keyCode === 77) {
--- assets/js/watch.js
+++ assets/js/watch.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
   let url = new URL(location.href);
   let code = url.searchParams.get("code");
-  let currentTime = url.searchParams.get("t");
-  let serverRoot =
-    "https://cors-anywhere.herokuapp.com/http://canbo.vinhuni.edu.vn";
-  // let serverRoot = "http://localhost:2018";
-  playFilm(code, serverRoot, currentTime);
-  // playFilmFake(code, serverRoot, currentTime);
+  let currentTime = url.searchParams.get("t") === null ? 0 : url.searchParams.get("t");
+  // let serverRoot = "https://cors-anywhere.herokuapp.com/http://canbo.vinhuni.edu.vn";
+  let serverRoot = "http://localhost:55418";
+  // playFilm(code, serverRoot, currentTime);
+  playFilmFake(code, serverRoot, currentTime);
   if (code !== "") {
     var item = arrayCartoonFilms
       .concat(arrayRetailFilms)
@@ -30,23 +29,7 @@ $(document).ready(function() {
   setUrlCommentFacebook(url);
 });
 
-$(".change-pixel").click(function() {
-  let videoplayer = document.getElementById("video-player");
-  let currentTimeOld = videoplayer.currentTime;
-  $("#video-player").attr("src", $(this).data("url"));
-  videoplayer.addEventListener(
-    "loadedmetadata",
-    function() {
-      videoplayer.currentTime = currentTimeOld;
-    },
-    false
-  );
-  videoplayer.play();
-});
 
-$("#toggleLight").click(function() {
-  toggleLight();
-});
 
 function setUrlCommentFacebook(url) {
   $(".fb-comments").attr(
@@ -78,17 +61,7 @@ function playFilm(filmCode, serverRoot, currentTime) {
           $("#1080").attr("data-url", item.url);
         }
       });
-      $("#video-player").attr("src", JSON.parse(result).medias[0].url);
-      let videoplayer = document.getElementById("video-player");
-      if (currentTime !== null) {
-        videoplayer.addEventListener(
-          "loadedmetadata",
-          function() {
-            this.currentTime = currentTime;
-          },
-          false
-        );
-      }
+      setUrlMedia(JSON.parse(result).medias[0].url, currentTime);
       togglePlayPause();
     }
   });
@@ -96,7 +69,7 @@ function playFilm(filmCode, serverRoot, currentTime) {
 
 function playFilmFake(filmCode, serverRoot, currentTime) {
   var result =
-    '{"requestId":"","status":1,"error":"","clientIp":"2405:4800:21c9:34f:7428:e7f2:4c00:6adf","clientOrgIp":"","clientIp6":false,"clientISP":"Unknow","clientProxyOut":false,"isMobile":false,"useVipServer":0,"notice":[],"message":"","reload":0,"reloadUrl":"","url":"phimmoi*3a7829ddb307f7abff8bda4665d007c3329198443fd27655f9aa0281a90740e227ac7f02e10481a77927d450cddd01d1","episodeId":159202,"episodeNumber":1,"episodePart":0,"episodeName":"","edition":"","serverId":"drive","backupOrder":3,"srtUrl":"","vttUrl":"","playTech":"auto","serverLanguage":"subtitle","language":"subtitle","filmId":6375,"isWaiting":0,"original":"","medias":[{"resolution":360,"type":"mp4","itag":"18","width":640,"height":360,"url":"https://r5---sn-npoe7ned.googlevideo.com/videoplayback?id=1847604d74d88d71&itag=18&source=webdrive&&requiressl=yes&mm=30&mn=sn-npoe7ned&ms=nxu&mv=u&pl=24&sc=yes&ttl=transient&ei=LbsxXKbNNNDH4ALUvor4DQ&susc=drp&app=fife&driveid=1lxpMsuQ1yxTA6kz4nMmFW2XgzttXTVHC&mime=video/mp4&dur=7070.290&lmt=1543116389409045&mt=1546762482&ip=123.31.30.133&ipbits=8&expire=1546770253&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,sc,ttl,ei,susc,app,driveid,mime,dur,lmt&signature=70156858C0D44D2F0702FDB4293B37DE38E34216E1F76815121D8DF3FD77354B.0949A8B0119DB4F0EFB995DD503A056E657D0F86E03AF348FDE8239A366DF15E&key=us0"},{"resolution":480,"type":"mp4","itag":"59","width":854,"height":480,"url":"https://r5---sn-npoe7ned.googlevideo.com/videoplayback?id=1847604d74d88d71&itag=59&source=webdrive&&requiressl=yes&mm=30&mn=sn-npoe7ned&ms=nxu&mv=u&pl=24&sc=yes&ttl=transient&ei=L7sxXLWGA5Dm4gKN8pnYAg&susc=drp&app=fife&driveid=1lxpMsuQ1yxTA6kz4nMmFW2XgzttXTVHC&mime=video/mp4&dur=7070.290&lmt=1543116710664610&mt=1546762482&ip=123.31.30.133&ipbits=8&expire=1546770255&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,sc,ttl,ei,susc,app,driveid,mime,dur,lmt&signature=89B9294C5D2EFBB0A4C990E7B9188699FBBABFB32DD7AABE78B5EF129D9E3432.1E199BD870F8A7ECD57CCFD840399BC46E7F0C5742776BBF65A2D8621D6E50E4&key=us0"},{"resolution":720,"type":"mp4","itag":"22","width":1280,"height":720,"url":"https://r5---sn-npoe7ned.googlevideo.com/videoplayback?id=1847604d74d88d71&itag=22&source=webdrive&&requiressl=yes&mm=30&mn=sn-npoe7ned&ms=nxu&mv=u&pl=24&sc=yes&ttl=transient&ei=L7sxXITUC5ns4wL74r2QDg&susc=drp&app=fife&driveid=1lxpMsuQ1yxTA6kz4nMmFW2XgzttXTVHC&mime=video/mp4&dur=7070.290&lmt=1543116035949255&mt=1546762482&ip=123.31.30.133&ipbits=8&expire=1546770255&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,sc,ttl,ei,susc,app,driveid,mime,dur,lmt&signature=560371C6E3029158C0BE4D4CFEAFAAE498B573FA210553B60BDD48A6E1BAE82E.9B34B056D061FDB7C78D250795F49BF93040ED189C028E6EBDAD20CE6FE74B37&key=us0"},{"resolution":1080,"type":"mp4","itag":"37","width":1920,"height":1080,"url":"https://r5---sn-npoe7ned.googlevideo.com/videoplayback?id=1847604d74d88d71&itag=37&source=webdrive&&requiressl=yes&mm=30&mn=sn-npoe7ned&ms=nxu&mv=u&pl=24&sc=yes&ttl=transient&ei=L7sxXOWPOYWf4AL__6zICA&susc=drp&app=fife&driveid=1lxpMsuQ1yxTA6kz4nMmFW2XgzttXTVHC&mime=video/mp4&dur=7070.290&lmt=1543116897480535&mt=1546762482&ip=123.31.30.133&ipbits=8&expire=1546770255&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,sc,ttl,ei,susc,app,driveid,mime,dur,lmt&signature=77D18BA7472E46ED68AB06A751C2CC62AD487A1351EFD9EB3510B45320D134B7.7F4DE0C0F5060C4596BF3F048B35133C70BD9BF6E99487395824BB99DD5A8812&key=us0"}],"mediasBk":[],"embedUrls":["U2FsdGVkX18nAEYMoQzeR0130j9RbawMjv0Z8ZP67uK0SqnLjda7IDXqiLMlY05P3Yf9qvLoTD3DYhWPEsr8AfPvlbSLJGIhlczpxaC7IqB5iI76VVsjTOLR2IQZZo635uk0EBOJZ7ul3yh+zbm6On8iAZ7KKuOvMYy5Mg4gVGoKCBXcbcRYKQaQK9uQIauYC6CZOPtzncJA01gN60oSx8HVyvXQZ38vRp4tgDWFKhu181zgniIoY22XlSf6RohQmb83cDylOzlN6S8UKdUc6kepnadItvi/etQIxJsa3KQ7l+XUqsOpYlGzjJUYvrX+mwH4e9kbXL4t/HzUTRkWY57Ig0IN0yhDdXgGQSVrFtwvlpm/7MQ/gnzWl6A4Gd0FkAEQovb/5oS6IDRjfdSfF03M1pYjv9jHlotf2F5sSJkQiU/FGUD1zmu4oq/WWgj/sZ8IuzVqIf5TFUiLNBonGGSoj3nS5eNQ3SPZBOVyqHOu2GVP3FOijgklo+urHx8dfol8cDZLNzPM1SaNdZrL4SXvKQkb3K9s3SUuAqJ0scDiBWYSkKfL4JV/eCFvnEhD"]}';
+    '{"requestId":"","status":1,"error":"","clientIp":"2405:4800:21c9:34f:1c9d:a46b:e758:c474","clientOrgIp":"","clientIp6":false,"clientISP":"Unknow","clientProxyOut":false,"isMobile":false,"useVipServer":0,"notice":[],"message":"","reload":0,"reloadUrl":"","url":"phimmoi*3a7829ddb307f7abff8bda4665d007c3329198443fd27655f9aa0281a90740e26084ab05e27012225d12adbdba0234f5","episodeId":175587,"episodeNumber":1,"episodePart":0,"episodeName":"","edition":"","serverId":"drive","backupOrder":3,"srtUrl":"","vttUrl":"","playTech":"auto","serverLanguage":"subtitle","language":"subtitle","filmId":7563,"isWaiting":0,"original":"","medias":[{"resolution":360,"type":"mp4","itag":"18","width":640,"height":360,"url":"https://r3---sn-npoe7nes.googlevideo.com/videoplayback?id=4e55647387a6831b&itag=18&source=webdrive&&requiressl=yes&mm=30&mn=sn-npoe7nes&ms=nxu&mv=u&pl=24&sc=yes&ttl=transient&ei=cFozXJvaB4fg4gLQu6mIBA&susc=drp&app=fife&driveid=1HYi3WT1Z59vQZ1oN6886r6T35AHwBCM1&mime=video/mp4&dur=7025.383&lmt=1546575772296117&mt=1546868514&ip=123.31.30.133&ipbits=8&expire=1546876560&sparams=ip,ipbits,expire,id,itag,source,requiressl,mm,mn,ms,mv,pl,sc,ttl,ei,susc,app,driveid,mime,dur,lmt&signature=ADBFEBEE3629000B08CBAA497EA9A3673A56492D5766ACF502B6D8B6FB176A24.324C0E69B1AB7769A9F96C1E61E1B1C8A5D8031FDF1C34F339A088D6FC935704&key=us0"}],"mediasBk":[],"embedUrls":["U2FsdGVkX18H5RaHJ63379+kGBa58sgmBFVkoQAbHxb94huWRnrwCuC59aMh26Y/33F5Nu7toF46MYgSp88PI5MEHy7hXcv9JfWwqv2FJlMZX0YPUlzBuXkZ5vp9nQ5V8GKDm/QA3iaWU43I2vf9H0uLUieYGCpPyE1O31L8d14vUrRatz2X0nI9ztpM0Mfi/uYHO6onoCRCOKS6pElGLLYttqaPwFb09hAHq+ZJ85i/UnsnIqpnDK67wIQI5GPu01vTugtooSp2EXGzuoHGZu7lwQbmCS9cnzy9SkiK93kjCgkXDiaC1JM0NNzODPwHoPhzyhKQdKBWdHutp0eZt0X3ZCSc05siER/69nfS2xk85PoOAdbLdUlW/L+aWQ9nULfM4J+MUWjJPN1GUl3U0Mie0hGL2mUoVWrcw27rOgYTTzm3JOJ77EjJfMGyeCH+uzFl9/ezHTx4ZRawaeWeecV+idqyu9/IABr8hmGgIjAmFfybt5F9aVZ4cPFcQCppQ9yI4UEL9zQ5E1t1cGVjVKQtooCRFy49RvHEDqFfaxiyENUYTzCRVpWx4mGcAcff"]}';
   $.each(JSON.parse(result).medias, function(i, item) {
     if (item.resolution === 360) {
       $("#360p").removeAttr("disabled");
@@ -115,16 +88,8 @@ function playFilmFake(filmCode, serverRoot, currentTime) {
       $("#1080").attr("data-url", item.url);
     }
   });
-  $("#video-player").attr("src", JSON.parse(result).medias[0].url);
-  if (currentTime !== null) {
-    document.getElementById("video-player").addEventListener(
-      "loadedmetadata",
-      function() {
-        this.currentTime = currentTime;
-      },
-      false
-    );
-  }
+  setUrlMedia(JSON.parse(result).medias[0].url, currentTime);
+  togglePlayPause();
 }
 
 function shareFilmFacebook() {
@@ -141,10 +106,3 @@ function shareFilmFacebook() {
   );
 }
 
-function toggleLight() {
-  if ($(".video-area").hasClass("light-off")) {
-    $(".video-area").removeClass("light-off");
-  } else {
-    $(".video-area").addClass("light-off");
-  }
-}
