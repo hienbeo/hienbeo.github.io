b3fa0d205410be05ed50b9b0bdef2c66fd1bbd28
Le Hoang Trình <hoangtrinh1610@gmail.com>
2019-01-07 11:10:32 +0700

    1

--- assets/data/retail-data.js
+++ assets/data/retail-data.js
@@ -2,17 +2,46 @@ var arrayRetailFilms = [];
 
 //append data
 // arrayRetailFilms.push({
-//     name: 'Mau đưa anh tôi đi dùm cái',
-//     eng: 'Go brother!',
+//     name: '',
+//     eng: '',
 //     type: 'retail',
+//     time: '',
 //     year: '',
-//     director: 'Scott Mann',
-//     img: 'mauduaanhtoididumcai.jpg',
+//     director: '',
+//     img: '.jpg',
 //     description: '',
 //     insertDate: '',
 //     ggCode: '',
-//     phimmoiCode: 'gau-paddington-2-i1-6377'
+//     phimmoiCode: ''
 // });
+
+arrayRetailFilms.push({
+    name: 'Mau đưa anh tôi đi dùm cái',
+    eng: 'Go brother!',
+    type: 'retail',
+    time: '115',
+    year: '2018',
+    director: 'Fen-Fen Chang',
+    img: 'mauduaanhtoididumcai.jpg',
+    description: 'Phim quanh một cặp anh trai em hay xung đột với nhau, một thằng anh trai không quan tâm đến gia đình, suốt ngày chọc phá em gái sẽ có cảm giác như thế nào??? Nên chiên hắn lên hay cho vào nồi hấp đây? Thời Miểu chỉ mong anh trai mình lập tức biến mất khỏi thế gian này, đến điều ước trong ngày sinh nhật đáng quý của mình cũng là ''Mau Đưa Anh Tôi Đi Giùm Cái''. Không ngờ điều ước ấy lại thành hiện thực. Anh trai mình trở thành anh trai của bạn thân cô Diệu Diệu, Thời Miểu hiểu được cảm giác của Diệu Diệu đồng thời trong lòng cũng thầm gào thét ''Đồ độc ác''! Thời Miểu trở thành đứa con duy nhất trong gia đình, cô cảm thấy cuộc sống vô cùng vô cùng tốt, nhưng lại phát hiện ra tuy ngoài mặt Thời Phân luôn vui vẻ với mọi người, trêu chọc em gái nhưng ẩn giấu bên trong lại có những bí mật không ai ngờ....',
+    insertDate: '',
+    ggCode: '',
+    phimmoiCode: 'mau-dua-anh-toi-di-gium-cai-7567'
+});
+
+arrayRetailFilms.push({
+    name: 'Ông anh "trời đánh"',
+    eng: 'Brother of the Year',
+    type: 'retail',
+    year: '2018',
+    time: '125',
+    director: 'Witthaya Thongyooyong',
+    img: '.jpg',
+    description: 'Ngay từ thời thơ ấu, cậu nhóc Chut luôn nghĩ là em bé trong bụng mẹ là em trai để có người chơi robot và đá bóng cùng mình. Tiếc là ông trời đã không lắng nghe nguyện vọng này khi để mẹ cậu hạ sinh một cô em gái "hở chút là mè nheo khóc lóc". ',
+    insertDate: '',
+    ggCode: '',
+    phimmoiCode: 'ong-anh-troi-danh-6942'
+});
 
 arrayRetailFilms.push({
     name: 'Trận bóng kinh hoàng',
@@ -25,35 +54,20 @@ arrayRetailFilms.push({
     description: 'Cho rằng gã anh trai phản bội Dimitri đang lẩn trốn đâu đó tại sân vận động Boleyn Ground nơi diễn ra trận bán kết tranh cúp Châu Âu giữa hai đội tuyển West Ham United và Dynamo FCC, Arkady , tên cầm đầu cuộc cách mạng tại bang Sukovia ở nước Nga đã phong tỏa toàn bộ sân vận động khiến nội bất xuất, ngoại bất nhập để truy tìm kẻ thù. Trong đám đông có mặt tại đó, Mike Knox và đứa cháu của mình Danni đã vô tình có mặt tại trận cầu sống còn và phải đối mặt với băng nhóm nguy hiểm của tên Arkady. Liệu Mike có kịp thời ngăn chăn Arkady trước khi hắn tìm thấy Dimitri? Sự sống của 35,000 người có mặt tại sân vận động và người dân nước Nga phụ thuộc cả vào anh.',
     insertDate: '',
     ggCode: '',
-    phimmoiCode: 'tran-bong-kinh-hoang-final-score-2018'
+    phimmoiCode: 'tran-bong-kinh-hoang-i1-7237'
 });
 
 arrayRetailFilms.push({
-    name: 'Aquaman: Đế vương Atlantis',
-    eng: 'Aquaman',
+    name: 'Đại sư huynh',
+    eng: 'Big Brother',
     type: 'retail',
-    time: '103',
+    time: '101',
     year: '2018',
-    director: 'James Wan',
+    director: 'Kan Ka Wai',
     img: 'aquaman.jpg',
-    description: 'Phim Aquaman (Aquaman: Đế Vương Atlantis) tiếp nối sau những sự kiện xảy ra trong Justice League – Liên Minh Công Lý. Theo đó, Arthur Curry/Aquaman bắt đầu trị vì vương quốc dưới biển sâu Atlantics, và đứng trong tình thế khó xử là những cư dân trên cạn luôn gây ô nhiễm môi trường toàn cầu trong khi người dân Atlantics luôn sẵn sàng để chiếm lấy đất liền. Bên cạnh đó, hải vương Aquaman còn phải đối mặt với những kẻ thù lăm le đe đọa nền hòa bình và yên ổn của vương quốc mình.',
+    description: 'Một giáo viên trung học mỗi ngày đều đặn tới trường gõ đầu trẻ cho đến một hôm…cậu học sinh do anh chủ nhiệm dính vào rắc rối với một băng đảng xã hội đen và bị bắt cóc. Đó cũng là lúc bí mật của“anh” thầy trước giờ luôn hiền lành, hết mực yêu thương học sinh và có tâm với nghề bị bộc lộ. Có vẻ như quá khứ máu lửa không buông tha “anh” thầy mà buộc anh phải trở lại con người xưa nhưng có khác là lần này không phải chiến đấu cho bản thân anh, mà cho những đứa trẻ mà anh luôn xem chúng như những đứa em trai.',
     insertDate: '',
     ggCode: '',
-    phimmoiCode: 'aquaman-de-vuong-atlantis-aquaman-2018'
+    phimmoiCode: 'dai-su-huynh-i1-7239'
 });
-
-
-// arrayRetailFilms.push({
-//     name: 'Đại sư huynh',
-//     eng: 'Big Brother',
-//     type: 'retail',
-    // time: '103',
-//     year: '2018',
-//     director: 'Khám Gia Vĩ',
-//     img: 'aquaman.jpg',
-//     description: 'Big Brother (Thầy Giáo Đại Ca): Một giáo viên trung học mỗi ngày đều đặn tới trường gõ đầu trẻ cho đến một hôm…cậu học sinh do anh chủ nhiệm dính vào rắc rối với một băng đảng xã hội đen và bị bắt cóc. Đó cũng là lúc bí mật của “anh” thầy trước giờ luôn hiền lành, hết mực yêu thương học sinh và có tâm với nghề bị bộc lộ. Có vẻ như quá khứ máu lửa không buông tha “anh” thầy mà buộc anh phải trở lại con người xưa nhưng có khác là lần này không phải chiến đấu cho bản thân anh, mà cho những đứa trẻ mà anh luôn xem chúng như những đứa em trai.',
-//     insertDate: '',
-//     ggCode: '',
-//     phimmoiCode: 'aquaman-de-vuong-atlantis-aquaman-2018'
-// });
 
